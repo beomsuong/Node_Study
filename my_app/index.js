63069f2ae81da28bfe0ea97611da779f11307e28
beomsuong <129273417+beomsuong@users.noreply.github.com>
2023-10-20 22:19:36 +0900

    10/20

--- my_app/index.js
+++ my_app/index.js
@@ -1,5 +1,26 @@
-import  { createServer } from 'http';
-import  { express } from 'express';
-const app= express;
+import  express from 'express';
+import path from 'path'
+import nunjucks from 'nunjucks'
 
-app.listen(3000);
+const app= express();
+const __dirname=path.resolve();
+
+app.set('view engine', 'html');
+
+nunjucks.configure('views',{
+    watch :true,
+    express:app
+})
+
+app.get('/',(req,res)=>{
+    //res.send('<h1>Hello Main page</h1>');
+    res.sendFile(__dirname + '/public/main.html');
+});
+
+app.get('/write',(req,res)=>{
+    res.render('write.html');
+});
+
+app.listen(3000,()=>{
+    console.log('Server is running');
+});
